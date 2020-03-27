30eaac89ec6ebe573f75067c2f53c7f37b6160b1
Ryan Fielder <ryan@FIELDER.LOCAL>
2020-03-27 14:36:32 -0500

    adjusted config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    D
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET
 }
